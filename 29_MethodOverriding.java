cef9fb18225fa84d80ca547ce0339faa4273aec9
RISHITHA DHANALA <r92009092@gmail.com>
2026-03-03 21:41:57 +0530

    Add Java program demonstrating method overriding

---
+++ 29_MethodOverriding.java
@@ -0,0 +1,19 @@
+class Parent {
+    void show() {
+        System.out.println("Parent class method");
+    }
+}
+
+class Child extends Parent {
+    @Override
+    void show() {
+        System.out.println("Child class method");
+    }
+}
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+        Parent obj = new Child();
+        obj.show();
+    }
+}
