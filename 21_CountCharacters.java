460c92f0c70bf50bf8d32a6d95f79deeb6a30a9e
RISHITHA DHANALA <r92009092@gmail.com>
2026-03-03 21:36:12 +0530

    Add Java program to count total characters in string

---
+++ 21_CountCharacters.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CountCharacters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        System.out.println("Total characters (including spaces) = " + str.length());
+    }
+}
